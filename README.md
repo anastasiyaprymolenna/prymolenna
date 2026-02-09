ff3b0ec7d58258e06cd5a0d345b75fc7af22a653
anastasiyaprymolenna <anastasiyawilson19@gmail.com>
2026-02-09 07:49:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# prymolenna
+Repo for my personal site
+
+[Visit the site!](https://anastasiyaprymolenna.github.io/prymolenna/)
---
+++ script.js
@@ -0,0 +1,5 @@
+﻿const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel, index) => {
+  panel.style.setProperty('--delay', `${index * 0.04}s`);
+});
